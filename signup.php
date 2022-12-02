efc885b2e17427eb8af08b491e0ad775ec5b62eb
yassir <yas.esraidi@gmail.com>
2022-12-02 15:43:36 +0100

    some design changes

--- signup.php
+++ signup.php
@@ -56,54 +56,58 @@
           <div class="card">
             <div class="card-body px-4 py-5 px-md-5 pt-0">
               <form id="signup" class="needs-validation" novalidate>
-                <div>
+              
                   <div class="d-flex  justify-content-center">
                     <img src="./assets/img/YouTrainTM.png" alt="logo" width="40%">
                   </div>
                   <h4 class="fw-bold mb-5">Sign up now</h4>
-                  <div class=" mb-4">
-                    <div class="form-outline">
-                      <label class="form-label" for="form3Example1">Username</label>
-                      <input type="text" name="name" id="name" class="form-control" required />
-                      <div class="valid-feedback">
-                        Looks Good!
+
+                  <div class="text-start">
+                      <div class=" mb-4">
+                      <div class="form-outline">
+                        <label class="form-label" for="form3Example1">Username</label>
+                        <input type="text" name="name" id="name" class="form-control" required />
+                        <div class="valid-feedback">
+                          Looks Good!
+                        </div>
+                        <div class="invalid-feedback">
+                          Username is required to create your account.
+                        </div>
                       </div>
+                    </div>
+                    <div class=" mb-4">
+                      <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Email address</label>
+                        <input type="email" name="email" id="email" class="form-control" required />
+                        <div class="valid-feedback">
+                          Looks Good!
+                        </div>
+                        <div class="invalid-feedback">
+                          Adress email is required to create your account.
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="form3Example3">Password</label>
+                      <input type="password" name="password" id="password" class="form-control" required />
                       <div class="invalid-feedback">
-                        Username is required to create your account.
+                        Enter a valid password !!
                       </div>
                     </div>
-                  </div>
-                  <div class=" mb-4">
-                    <div class="form-outline">
-                      <label class="form-label" for="form3Example2">Email address</label>
-                      <input type="email" name="email" id="email" class="form-control" required />
-                      <div class="valid-feedback">
-                        Looks Good!
-                      </div>
+
+                    <!-- Confirm Password input -->
+                    <div class="form-outline mb-4">
+                      <label class="form-label" for="form3Example4">Confirm Password</label>
+                      <input type="password" name="confirm_password" id="confirm_password" class="form-control" required />
                       <div class="invalid-feedback">
-                        Adress email is required to create your account.
+                        Passwords doesn't match !!
                       </div>
+
                     </div>
                   </div>
-
-                  <!-- Password input -->
-                  <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example3">Password</label>
-                    <input type="password" name="password" id="password" class="form-control" required />
-                    <div class="invalid-feedback">
-                      Enter a valid password !!
-                    </div>
-                  </div>
-
-                  <!-- Confirm Password input -->
-                  <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example4">Confirm Password</label>
-                    <input type="password" name="confirm_password" id="confirm_password" class="form-control" required />
-                    <div class="invalid-feedback">
-                      Passwords doesn't match !!
-                    </div>
-
-                  </div>
+                  
 
 
                   <!-- Submit button -->
@@ -111,9 +115,9 @@
                     Sign up
                   </button>
               </form>
-              <div class="mt-2">
-                <p>You alraedy have an account? <a class="text-decoration-none" href="login.php">Login</a></p>
-              </div>
+                <div class="mt-2">
+                  <p>You alraedy have an account? <a class="text-decoration-none" href="login.php">Login</a></p>
+                </div>
             </div>
           </div>
         </div>
