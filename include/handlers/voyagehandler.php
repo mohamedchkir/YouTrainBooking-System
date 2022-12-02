9e3a9d207c1feadc4b3d7e200dfb3497b43afb99
mounir <mounir/elbakkali1@etu.uae.ac.ma>
2022-12-02 17:23:08 +0100

    I started writting besiness logic : Voyage(Model/View/Controller)

--- include/handlers/voyagehandler.php
+++ include/handlers/voyagehandler.php
@@ -3,6 +3,7 @@
 include_once("../autoloader.php");
 //include_once("../../class/CityController.class.php");
 if (isset($_POST["suggestions"])) getSuggestions();
+if (isset($_POST["search"])) getAvailableTrips();
 
 
 
@@ -12,20 +13,44 @@ if (isset($_POST["suggestions"])) getSuggestions();
 function getSuggestions()
 {
  $sugg = $_POST["suggestions"];
- //$city = new CityController();
- //$cities = $city->getCities();
- $cities = array("casa", "tanger", "tetouen", "castia", "rabat", "sale", "kenitra");
+ $city = new CityController();
+ $cities = $city->getCities();
+ // foreach ($cities as $city) {
+ //  print_r($city["ville"]);
+ // }
+
+ // exit();
+ //$cities = array("casa", "tanger", "tetouen", "castia", "rabat", "sale", "kenitra");
  $condition = true;
 
  foreach ($cities as $c) {
   if (empty($sugg)) {
    $condition = true;
   } else {
-   $condition = strpos(strtolower($c), strtolower($sugg)) !== false;
+   $condition = strpos(strtolower($c['ville']), strtolower($sugg)) !== false;
   }
   if ($condition) {
-   echo "<input type='button' class='btn w-100 border-bottom' onclick='putValue(this)'  value='$c'>";
+   echo "<input type='button' class='btn w-100 border-bottom' onclick='putValue(this)'  value='" . $c["ville"] . "'>";
   }
  }
  //echo json_encode($cities);
+}
+
+
+function getAvailableTrips()
+{
+    $gare_depart = $_POST["gare_depart"];
+    $gare_distination = $_POST["gare_distination"];
+    $date_depart = $_POST["date_depart"];
+    $voyageCtr = new VoyageController();
+
+
+    $available = $voyageCtr->getAvailableTrains($gare_depart,$gare_distination,$date_depart);
+    echo "<pre>";
+    print_r($available);
+    echo "</pre>";
+    echo "<pre>";
+    echo $gare_depart."    ".$gare_distination."      ".$date_depart;
+    echo "</pre>";
+    exit();
 }
