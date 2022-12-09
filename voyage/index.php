ca4f3259e72940c0477233c39d771ccb6ddf08b5
rachiddaoudi <rachiddaoudi533@gmail.com>
2022-12-09 15:57:49 +0100

    modifier dashboard

--- voyage/index.php
+++ voyage/index.php
@@ -20,13 +20,13 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="../assets/css/style3.css">
+  <!-- <link rel="stylesheet" href="../assets/css/style3.css"> -->
   <!-- Font Awesome-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-  <section style="height:94vh ;" class="container">
+  <section class="container">
     <div class="w-75 mb-5 row">
       <div class="col-8">
       <form action="../include/handlers/voyagehandler.php" class="needs-validation" method="POST" novalidate>
@@ -74,7 +74,7 @@
             <input type="datetime-local" class="form-control" id="datetime" name="datetime" required>
           </div>
           <div class="mt-3 d-flex justify-content-center">
-            <button type="submit" class="btn text-light px-5" style="background-color:var(--aqua);border-radius: 20px;" name="saveVoyage">Save</button>
+            <button type="submit" class="btn text-light px-5 bg-primary" style="background-color:var(--aqua);border-radius: 20px;" name="saveVoyage">Save</button>
           </div>
         </form>
       </div>
